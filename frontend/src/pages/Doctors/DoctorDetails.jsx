251f2d225840b44ba9d2a3efabc5f11377f8e584
Indraneel Das <dasindraneel3002@gmail.com>
2024-06-12 11:48:15 +0530

    Add Feedback and FeedbackForm components with star rating feature and import avatar icon

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -18,10 +18,10 @@ const DoctorDetails = () => {
               </figure>
               <div>
                 <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
-                  Skin Specialist
+                  Sr. Dentist
                 </span>
                 <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
-                  Dr. Kinnor Das
+                  Dr. Nandita Singh
                 </h3>
                 <div className="flex items-center gap-[6px]">
                   <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
