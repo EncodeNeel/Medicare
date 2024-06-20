4fab90c2886c073617127563a3aa003f28d87878
Indraneel Das <dasindraneel3002@gmail.com>
2024-06-21 00:35:51 +0530

    Add doctor and user routes, controllers for CRUD operations and profile management; integrate authentication and authorization

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -4,6 +4,7 @@ import {
   deleteDoctor,
   getSingleDoctor,
   getAllDoctors,
+  getDoctorProfile,
 } from "../Controllers/doctorController.js"; // Ensure this path is correct
 import { authenticate, restrict } from "../auth/verifyToken.js";
 
@@ -17,6 +18,8 @@ router.use("/:doctorId/reviews", reviewRouter);
 router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
 router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
 router.get("/:id", getSingleDoctor);
-router.get("/", getAllDoctors); // Changed route to '/' for getting all users
+router.get("/", getAllDoctors);
+
+router.get("/profile/me", authenticate, restrict(["doctor"]), getDoctorProfile);
 
 export default router;
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -4,13 +4,22 @@ import {
   deleteUser,
   getSingleUser,
   getAllUsers,
+  getUserProfile,
+  getMyAppointments,
 } from "../Controllers/userController.js"; // Ensure this path is correct
 
 import { authenticate, restrict } from "../auth/verifyToken.js";
 const router = express.Router();
 
-router.put("/:id", restrict(["patient"]), updateUser);
-router.delete("/:id", restrict(["patient"]), deleteUser);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
+router.get("/profile/me", authenticate, restrict(["patient"]), getUserProfile);
+router.get(
+  "/appointments/my-appointments",
+  authenticate,
+  restrict(["patient"]),
+  getMyAppointments
+);
 router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
 router.get("/", authenticate, restrict(["admin"]), getAllUsers); // Ensure authentication and restriction for getting all users
 
