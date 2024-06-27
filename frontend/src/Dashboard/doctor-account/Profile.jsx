65f123e78f6b8d137968b61cdc5a7f5b1287f970
Indraneel Das <dasindraneel3002@gmail.com>
2024-06-27 20:25:38 +0530

    Add phone and bio fields to Profile form

--- frontend/src/Dashboard/doctor-account/Profile.jsx
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import { useState } from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+        Profile Information
+      </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name*</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Full Name"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="form__input"
+            readOnly
+            aria-readonly
+            disabled="true"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input
+            type="number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Phone number"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Bio*</p>
+          <input
+            type="text"
+            name="bio"
+            value={formData.data}
+            onChange={handleInputChange}
+            placeholder="Bio"
+            className="form__input"
+            maxLength={100}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
