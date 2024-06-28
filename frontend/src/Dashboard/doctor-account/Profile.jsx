21b52da2b5b82e357a77f71d0790b1d235bdab2a
Indraneel Das <dasindraneel3002@gmail.com>
2024-06-29 01:15:50 +0530

    Add qualifications, experiences, time slots, and photo upload in Profile component

--- frontend/src/Dashboard/doctor-account/Profile.jsx
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Profile = () => {
   const [formData, setFormData] = useState({
@@ -9,9 +10,15 @@ const Profile = () => {
     gender: "",
     specialization: "",
     ticketPrice: 0,
-    qualifications: [],
-    experiences: [],
-    timeSlots: [],
+    qualifications: [
+      { startingDate: "", endingDate: "", degree: "", university: "" },
+    ],
+    experiences: [
+      { startingDate: "", endingDate: "", position: "", hospital: "" },
+    ],
+    timeSlots: [{ day: "", startingTime: "", endingTime: "" }],
+    about: "",
+    photo: null,
   });
 
   const handleInputChange = (e) => {
@@ -20,6 +27,37 @@ const Profile = () => {
       ...prevData,
       [name]: value,
     }));
+  };
+
+  const handleFileInputChange = (e) => {};
+
+  const updateProfileHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  // reusable function for adding item
+  const addItem = (key, item) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [key]: [...prevFormData[key], item],
+    }));
+  };
+
+  //reusable input change function
+
+  const addQualification = (e) => {
+    e.preventDefault();
+
+    addItem("qualifications", {
+      startingDate: "",
+      endingDate: "",
+      degree: "",
+      university: "",
+    });
+  };
+
+  const handleQualificationChange = (event, index) => {
+    handleReusableInputChangeFunc("qualifications", index, event);
   };
 
   return (
@@ -120,6 +158,232 @@ const Profile = () => {
               />
             </div>
           </div>
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Qualifications*</p>
+          {formData.qualifications?.map((item, index) => (
+            <div key={index}>
+              <div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label">Starting Date*</p>
+                    <input
+                      type="date"
+                      name="startingDate"
+                      value={item.startingDate}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label">Ending Date*</p>
+                    <input
+                      type="date"
+                      name="endingDate"
+                      value={item.endingDate}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label">Degree*</p>
+                    <input
+                      type="text"
+                      name="degree"
+                      value={item.degree}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label">University*</p>
+                    <input
+                      type="text"
+                      name="university"
+                      value={item.university}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+
+                <button className="bg-red-600 p-2 rounded-full text-white text-[18px] mt-2 mb-[30px]">
+                  <AiOutlineDelete />
+                </button>
+              </div>
+            </div>
+          ))}
+
+          <button
+            onClick={addQualification}
+            className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer"
+          >
+            Add Qualification
+          </button>
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Experiences*</p>
+          {formData.experiences?.map((item, index) => (
+            <div key={index}>
+              <div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label">Starting Date*</p>
+                    <input
+                      type="date"
+                      name="startingDate"
+                      value={item.startingDate}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label">Ending Date*</p>
+                    <input
+                      type="date"
+                      name="endingDate"
+                      value={item.endingDate}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 gap-5">
+                  <div>
+                    <p className="form__label">Position*</p>
+                    <input
+                      type="text"
+                      name="position"
+                      value={item.position}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label">Hospital*</p>
+                    <input
+                      type="text"
+                      name="hospital"
+                      value={item.hospital}
+                      className="form__input"
+                    />
+                  </div>
+                </div>
+
+                <button className="bg-red-600 p-2 rounded-full text-white text-[18px] mt-2 mb-[30px]">
+                  <AiOutlineDelete />
+                </button>
+              </div>
+            </div>
+          ))}
+
+          <button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+            Add Experience
+          </button>
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Time Slots*</p>
+          {formData.timeSlots?.map((item, index) => (
+            <div key={index}>
+              <div>
+                <div className="grid grid-cols-2 md:grid-cols-4 mb-[30px] gap-5">
+                  <div>
+                    <p className="form__label">Day*</p>
+                    <select
+                      name="day"
+                      value={item.day}
+                      className="form__input py-3.5"
+                    >
+                      <option value="">Select</option>
+                      <option value="saturday">Saturday</option>
+                      <option value="sunday">Sunday</option>
+                      <option value="monday">Monday</option>
+                      <option value="tuesday">Tuesday</option>
+                      <option value="wednesday">Wednesday</option>
+                      <option value="thursday">Thrusday</option>
+                      <option value="friday">Friday</option>
+                    </select>
+                  </div>
+                  <div>
+                    <p className="form__label">Staring Time*</p>
+                    <input
+                      type="time"
+                      name="startingTime"
+                      value={item.startingTime}
+                      className="form__input"
+                    />
+                  </div>
+                  <div>
+                    <p className="form__label">Ending Time*</p>
+                    <input
+                      type="time"
+                      name="endingTime"
+                      value={item.endingTime}
+                      className="form__input"
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <button className="bg-red-600 p-2 rounded-full text-white text-[18px]  cursor-pointer mt-6">
+                      <AiOutlineDelete />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          <button className="bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer">
+            Add TimeSlot
+          </button>
+        </div>
+        <div className="mb-5">
+          <p className="form__label">About*</p>
+          <textarea
+            name="about"
+            rows={5}
+            value={formData.about}
+            placeholder="Write about you"
+            onChange={handleInputChange}
+            className="form__input"
+          ></textarea>
+        </div>
+
+        <div className="mb-5 flex items-center gap-3">
+          {formData.photo && (
+            <figure
+              className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor
+                flex items-center justify-center overflow-hidden"
+            >
+              <img
+                src={formData.photo}
+                alt=""
+                className="w-full rounded-full"
+              />
+            </figure>
+          )}
+
+          <div className="relative w-[130px] h-[50px]">
+            <input
+              type="file"
+              name="photo"
+              id="customFile"
+              onChange={handleFileInputChange}
+              accept=".jpg, .png"
+              className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+            />
+
+            <label
+              htmlFor="customFile"
+              className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+            >
+              Upload Photo
+            </label>
+          </div>
+        </div>
+
+        <div className="mt-7">
+          <button
+            type="submit"
+            oncLICK={updateProfileHandler}
+            className="bg-PrimaryColor text-white text-[18px] leading-[30px] w-full py-3 px-4 rounded-lg"
+          >
+            Update Profile
+          </button>
         </div>
       </form>
     </div>
