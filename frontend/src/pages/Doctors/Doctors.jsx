9bad1e129e4d9c70d4178d1f73a4ed88a33b400d
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-04 14:52:29 +0530

    added doctor fetching

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,8 +1,13 @@
 import DoctorCard from "./../../components/Doctors/DoctorCard";
 import { doctors } from "./../../assets/data/doctors";
 import Testimonial from "../../components/Testimonal/Testimonial";
+import { BASE_URL } from "./../../config";
+import useFetchData from "./../../hooks/useFetchData";
+import Loader from "../../components/Loader/Loading";
+import Error from "../../components/Error/Error";
 
 const Doctors = () => {
+  const { data: doctors, loading, error } = useFetchData(`${BASE_URL}/doctors`);
   return (
     <>
       <section className="bg-[#fff9ea]">
@@ -23,11 +28,15 @@ const Doctors = () => {
 
       <section>
         <div className="container">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
-            {doctors.map((doctor) => (
-              <DoctorCard key={doctor.id} doctor={doctor} />
-            ))}
-          </div>
+          {loading && <Loader />}
+          {error && <Error />}
+          {!loading && !error && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
+              {doctors.map((doctor) => (
+                <DoctorCard key={doctor.id} doctor={doctor} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
       <section>
